0b1e41ad07322b715718279c8db5e62c2f0a6b81
noFrostoo <daniel.lipniacki@protonmail.com>
2022-01-13 16:23:11 +0100

    first

---
+++ psi_project/tcp/Sender.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+async def tcp_echo_client(fileName):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(f'Started reading file')
+    await asyncio.sleep(7)
+    with open(fileName, "rb") as f:
+        data = f.read()
+
+    print(f'Send: ')
+    writer.write(data)
+
+    print('Close the connection')
+    writer.close()
+
+asyncio.run(tcp_echo_client('testFile.test'))
