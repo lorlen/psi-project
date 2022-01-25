e7f9fbc00b63129496815b45356ffb553f2e2589
Filip Lew <46330610+lorlen@users.noreply.github.com>
2022-01-25 10:39:34 +0100

    task proxy (#11)

--- psi_project/core/utils.py
+++ psi_project/core/utils.py
@@ -1,3 +1,6 @@
+import asyncio
+
+
 class Singleton(type):
     _instances = {}
 
@@ -5,3 +8,9 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def task_proxy(coro):
+    async def task_proxy_helper(*args, **kwargs):
+        asyncio.create_task(coro(*args, **kwargs))
+    return task_proxy_helper
